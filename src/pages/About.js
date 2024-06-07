1211d488ad0d9e3a2fd512f5d64bb03d27379237
Ibrahim Shah <104ibrahimshah@gmail.com>
2024-06-07 16:28:45 -0700

    base requirements implemented

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,29 @@
-import React, { Component } from 'react';
-  
+import React, { Component } from "react";
+import "./About.css";
+import logo from '../assets/pfp.jpg';
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div className="split left">
+          <div className="centered">
+            <img
+              className="profile_image"
+              src={logo}
+              alt="Profile Pic"
+            ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Your Name</div>
+            <div className="brief_description">
+              // Details about you goes here
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import AddTodo from "../component/AddTodo";
+import Todos from "../component/todos";
+
+//Import hte home.css file
+import "./Home.css";
 
 class Home extends Component {
   // A default state of this component with an empty list of todos.
   constructor() {
     super();
     this.state = {
+      todos: []
       // create your empty list here call it todos.
     };
   }
@@ -27,7 +33,9 @@ class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <p> Replace this</p>
+        <Todos todos={this.state.todos} />
+        {/* The AddTodo component is called here with the addTodo function passed as a prop */}
+        <AddTodo addTodo={this.addTodo} />
         <h1>Todo's </h1>
       </div>
     );
